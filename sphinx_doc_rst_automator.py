d0eb89e56a6e28a85785581e82c64f5969f37f8b
Jacob Johnson <jakeymacaroni@gmail.com>
2023-10-09 10:37:31 -0600

    Update sphinx_doc_rst_automator.py

--- sphinx_doc_rst_automator.py
+++ sphinx_doc_rst_automator.py
@@ -31,13 +31,6 @@ class Main:
 
         choose_folder_frame = tk.Frame(self.root)
         choose_folder_frame.grid(row=1, column=1)
-        
-        python_folder_choice_button = tk.Button(choose_folder_frame,text="Select Python Folder",
-                                                command=self.select_python_folder_dialog)
-        python_folder_choice_button.grid(row=0)
-
-        self.chosen_python_folder_label = tk.Label(choose_folder_frame,text="No Python folder chosen")
-        self.chosen_python_folder_label.grid(row=1)
 
         choose_source_folder_button = tk.Button(choose_folder_frame, text="Choose source folder",
                                          command=self.select_source_folder_dialog)
@@ -46,12 +39,12 @@ class Main:
         self.chosen_source_folder_label = tk.Label(choose_folder_frame, text="No folder chosen")
         self.chosen_source_folder_label.grid(row=3)
 
-        chosen_destination_folder_button = tk.Button(choose_folder_frame, text="Choose destination folder",
-                                                          command=self.select_destination_folder_dialog)
-        chosen_destination_folder_button.grid(row=4)
+        # chosen_destination_folder_button = tk.Button(choose_folder_frame, text="Choose destination folder",
+        #                                                   command=self.select_destination_folder_dialog)
+        # chosen_destination_folder_button.grid(row=4)
 
-        self.chosen_destination_label = tk.Label(choose_folder_frame, text="No folder chosen")
-        self.chosen_destination_label.grid(row=5)
+        # self.chosen_destination_label = tk.Label(choose_folder_frame, text="No folder chosen")
+        # self.chosen_destination_label.grid(row=5)
 
         populate_button = tk.Button(self.root, text="Populate Files", command=self.populate_files)
         populate_button.grid(row=3, column=0,columnspan=2,pady=10)
@@ -128,12 +121,12 @@ class Main:
         self.files_field.insert(tk.END, folder_output)
         self.files_field.update()
 
-    def select_python_folder_dialog(self):
-        self.chosen_python_folder = filedialog.askdirectory(initialdir="/")
-        self.chosen_python_folder_label.configure(text=f"Folder chosen: {self.chosen_python_folder}")
-        
-
     def populate_files(self):
+        docs_path = os.path.dirname(os.path.dirname(self.chosen_source_folder)) + "/docs"
+        os.mkdir(docs_path)
+        os.mkdir(docs_path + "/source")
+
+
         if self.chosen_source_folder == "":
             tk_mb.showinfo(message="Error: no source folder chosen")
 
@@ -159,7 +152,7 @@ class Main:
 
                         if file_name != "":
                             # Write to file
-                            with open(f"{self.chosen_destination_folder}/{file_name}", "w+") as file:
+                            with open(f"{docs_path}/source/{file_name}", "w+") as file:
                                 file.write(current_output)
 
                         file_name = f"{line}.rst"
@@ -173,24 +166,22 @@ class Main:
                 self.rst_files.append(file_name)
                 current_output = (f"Documentation\n-------------\n{current_output}")
 
-            with open(f"{self.chosen_destination_folder}/{file_name}", "w+") as file:
+            with open(f"{docs_path}/source/{file_name}", "w+") as file:
                 file.write(current_output)
 
             with open("index_contents.txt", "r") as index_source_file:
-                with open(f"{self.chosen_destination_folder}/index.rst", "w+") as file:
+                with open(f"{docs_path}/source/index.rst", "w+") as file:
                     file.write(index_source_file.read())
 
             with open("getting_started_contents.txt", "r+") as getting_started_source_file:
-                with open(f"{self.chosen_destination_folder}/getting_started.rst", "w+") as file:
+                with open(f"{docs_path}/source/getting_started.rst", "w+") as file:
                     file.write(getting_started_source_file.read())
 
-            with open(f"{self.chosen_destination_folder}/generate.bat", "w+") as file:
+            with open(f"{docs_path}/generate.bat", "w+") as file:
                 directory = os.path.dirname(self.chosen_source_folder)
-                #directory = f"../{str(directory).split('/')[-1]}"
-                file.write(f"{self.chosen_python_folder}\scripts\sphinx-autobuild source build/html --watch {directory}\npause")
+                file.write(f"sphinx-autobuild source build/html --watch {directory}\npause")
 
-
-            with open(f"{self.chosen_destination_folder}/index.rst", 'r') as index_file:
+            with open(f"{docs_path}/source/index.rst", 'r') as index_file:
                 with open("temp_file.rst","w") as temp_file:
                     for line in index_file:
                         temp_file.write(line)
@@ -201,7 +192,11 @@ class Main:
                                 print(file_name)
                                 temp_file.write(f"   {file_name}\n")
 
-            os.replace("temp_file.rst", f"{self.chosen_destination_folder}/index.rst")
+            with open(f"{docs_path}/source/conf.py", 'w+') as file:
+                with open("conf_contents.txt","r") as conf_source_file:
+                    file.write(conf_source_file.read())
+
+            os.replace("temp_file.rst", f"{docs_path}/source/index.rst")
 
             tk_mb.showinfo(message="Files Populated")
 
